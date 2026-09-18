6144f5feac145442135863ccc7b6f0f96cee87a4
GregSolide <adriengregoiresolide@gmail.com>
2026-09-17 21:13:45 -0400

    previous commit description

--- Backend/Logic.py
+++ Backend/Logic.py
@@ -7,6 +7,4 @@ s3_client = boto3.client("s3")
 lambda_client = boto3.client("lambda")
 s3_client.create_bucket(Bucket="uploads")
 
-with open("FILE _NAME", "rb") as f:
-    if f.name.split(".")[1] == "csv" :
-        s3_client.upload_fileobj(f, "uploads", "OBJECT_NAME", Callback= ProgressPercentage('FILE_NAME'))
+
--- Frontend/routing.py
+++ Frontend/routing.py
@@ -1,11 +1,20 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,url_for
+import csv
 
 app = Flask(__name__)
 
 @app.route("/",methods= ["GET","POST"])
 def hello_world():
-    return render_template("")
-
+    return render_template("Home.html")
+@app.route("/file_proccessed",methods=["POST"])
+def get_filed():
+    if request.method == "POST":
+        document = request.files["file"]
+        document.save("Frontend/uploads/"+document.filename)
+        with open ("Frontend/uploads/"+document.filename,"r") as f:
+                    data = csv.reader(f)
+                    
+    return render_template("test.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
