161908c8548ef0277b3a77abe2db2dc9fba361c3
GregSolide <adriengregoiresolide@gmail.com>
2026-09-17 20:52:39 -0400

    front page added + reading csv

--- Frontend/routing.py
+++ Frontend/routing.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route("/",methods= ["GET","POST"])
 def hello_world():
-    return render_template("Home.html")
+    return render_template("")
+
+
+if __name__ == "__main__":
+    app.run()
